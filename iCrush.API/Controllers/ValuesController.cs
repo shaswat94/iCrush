f5b8244f61bf0cdfe99238e216efb670153c546c
Shaswat Swarup <shaswatswarup.1994@gmail.com>
2018-12-24 12:28:25 +0530

    Initial Commit

---
+++ iCrush.API/Controllers/ValuesController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using iCrush.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace iCrush.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _dbContext;
+        public ValuesController(DataContext dbContext)
+        {
+            this._dbContext = dbContext;
+
+        }
+        // GET api/values
+
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await _dbContext.Values.ToListAsync();
+
+            return Ok(values);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value = await _dbContext.Values.FirstOrDefaultAsync( v => v.Id == id);
+            
+            return Ok(value);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ iCrush.API/Modules/Value.cs
@@ -0,0 +1,9 @@
+namespace iCrush.API.Modules
+{
+    public class Value
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
