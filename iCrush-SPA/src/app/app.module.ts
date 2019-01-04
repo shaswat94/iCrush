64fdd93a47d06e5c0c805ea7e55e26e5e80f2489
Shaswat Swarup <shaswatswarup.1994@gmail.com>
2019-01-04 19:21:24 +0530

    End of Edit Member Section. S10

--- iCrush-SPA/src/app/app.module.ts
+++ iCrush-SPA/src/app/app.module.ts
@@ -37,6 +37,9 @@ import { AuthGuard } from './_guards/auth.guard';
 import { UserService } from './_services/user.service';
 import { MemberDetailResolver } from './_resolver/member-detail.resolver';
 import { MemberListResolver } from './_resolver/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolver/member-edit.resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 
 
 
@@ -54,7 +57,8 @@ export function tokenGetter() {
       ListsComponent,
       MessagesComponent,
       MemberCardComponent,
-      MemberDetailComponent
+      MemberDetailComponent,
+      MemberEditComponent
    ],
    imports: [
       BrowserModule,
@@ -87,7 +91,9 @@ export function tokenGetter() {
       AuthGuard,
       UserService,
       MemberDetailResolver,
-      MemberListResolver
+      MemberListResolver,
+      MemberEditResolver,
+      PreventUnsavedChanges
    ],
    bootstrap: [
       AppComponent
--- iCrush.API/Helpers/AutomapperProfiles.cs
+++ iCrush.API/Helpers/AutomapperProfiles.cs
@@ -14,12 +14,16 @@ namespace iCrush.API.Helpers
             }).ForMember( dest => dest.Age, opt => {
                 opt.ResolveUsing( d => d.DateOfBirth.CalculateAge());
             });
+
             CreateMap<User, UserForDetailedDto>().ForMember( dest => dest.PhotoUrl, opt => {
                 opt.MapFrom(src => src.Photos.FirstOrDefault( p => p.IsMain ).Url);
             }).ForMember( dest => dest.Age, opt => {
                 opt.ResolveUsing( d => d.DateOfBirth.CalculateAge());
             });
+
             CreateMap<Photo, PhotosForDetailedDto>();
+
+            CreateMap<UserForUpdateDto, User>().ForMember( dest => dest.Id, opt => opt.Ignore());
         }
     }
 }
